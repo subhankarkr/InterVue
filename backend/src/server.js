b285791331663ccdd60da25297f5e1b95b948b51
Subhankar kumar <subhankarkr39@gmail.com>
2026-03-23 17:36:45 +0530

    auth and  db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,23 @@
 import express from "express"
 import {ENV} from "./lib/env.js"
 import path from "path"
+import cors from "cors"
+import serve from "@inngest/express"
 import { connectDB } from "./lib/db.js";    
+import { functions } from "./lib/inngest.js"
 const app =express();
 const __dirname = path.resolve()
 
 console.log(ENV.PORT)
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+app.use(express.json());
+
+app.use("/api/inngest",serve({client:inngest,functions}));
 app.get("/health",(req,res)=>{
     res.status(200).json({msg:"app is up and running"});
 });
 app.get("/books",(req,res)=>{
-    res.status(200).json({msg:"this is books endpoint  "});
+    res.status(200).json({msg:"this is books endpoint   "});
 });
 if(ENV.NODE_ENV == "production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")))
