29d0c4d1882d9ea035f9334914b35c1d66229ac5
Subhankar kumar <subhankarkr39@gmail.com>
2026-04-17 13:40:32 +0530

    minors fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,27 +2,30 @@ import express from "express"
 import { ENV } from "./lib/env.js"
 import path from "path"
 import cors from "cors"
+import { clerkMiddleware } from "@clerk/express"
 import { serve } from "inngest/express"
 import { inngest } from "./lib/inngest.js"
 import { connectDB } from "./lib/db.js";
 import { functions } from "./lib/inngest.js"
 const app = express();
 import { fileURLToPath } from 'url';
+import chatRoutes from "./routes/chatRoutes.js";
+import { protectRoute } from "./middlewares/protectRoute.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 console.log(ENV.PORT)
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(express.json());
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware());
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat",chatRoutes);
 app.get("/health", (req, res) => {
     res.status(200).json({ msg: "app is up and running" });
 });
-app.get("/books", (req, res) => {
-    res.status(200).json({ msg: "this is books endpoint   " });
-});
+
 if (ENV.NODE_ENV == "production") {
     app.use(express.static(path.join(__dirname, "../../frontend/dist")))
     app.get("/{*any}", (req, res) => {
