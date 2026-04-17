e5e440d6f2fc54bae43887c0b2c9e601e3ea1fb0
Subhankar kumar <subhankarkr39@gmail.com>
2026-04-17 12:57:55 +0530

    implement chat routes and authentication middleware using Clerk

---
+++ backend/src/middlewares/protectRoute.js
@@ -0,0 +1,22 @@
+import { requireAuth } from "@clerk/express";
+import User from "../models/User";
+export const protectRoute = [
+    requireAuth(), async (req, res, next) => {
+        try {
+            const clerkId = req.auth.userId;
+            if (!clerkId) {
+                return res.status(401).json({ msg: "Unauthorized" });
+            }
+            const user = await User.findOne({ clerkId });
+            if (!user) {
+                return res.status(404).json({ msg: "User not found" });
+            }
+            req.user = user;
+            next();
+        }
+        catch (err) {
+            console.log("Error protecting route ", err);
+            return res.status(500).json({ msg: "Internal server error" });
+        }
+    }
+]
